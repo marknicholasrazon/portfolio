987cfa940967a4d0b1562a93d602b29fa2feeea8
marknicholasrazon <razonmarknicholas.cdlb@gmail.com>
2022-12-23 16:29:37 +0800

    Updates

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="author" content="Mark Nicholas Razon">
+    <meta name="description" content="I like to build stuff and trying out new technologies. 🚀">
+    <meta name="generator" content="Mark Nicholas Razon">
+    <meta name="robots" content="noindex, nofollow">
+    <meta property="og:locale" content="es_ES">
+    <meta property="og:type" content="website">
+    <title>Mark Nicholas Razon - Full Stack Developer</title>
+    <link rel="icon" href="%PUBLIC_URL%/favicon.png" />
+    <link rel="apple-touch-icon" href="%PUBLIC_URL%/favicon.png" />
+    <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
+</head>
+<body>
+    <div id="root"></div>
+</body>
+</html>
